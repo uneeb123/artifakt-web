319f3d2afb65e807dc1f665437746b36b124e89a
Uneeb Agha <uneeb.agha@gmail.com>
2018-10-05 14:24:09 -0400

    register page starting to take up some shape

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import logo from './logo/logo_black.svg'; 
+
 export default class Login extends Component {
   constructor(props) {
     super(props);
@@ -19,22 +21,46 @@ export default class Login extends Component {
     event.preventDefault();
   }
 
+  _registerInfo = () => {
+    return (
+      <div>
+        <img src={logo} className="Login-logo" alt="logo" />
+        <ul className="list-unstyled">
+          <li className="Login-info-li">discover elite players</li>
+          <li className="Login-info-li">find rare collectibles</li>
+          <li className="Login-info-li">power-up your inventory</li>
+        </ul>
+      </div>
+    );
+  }
+
   _registerForm = () => {
+    let allow = this.props.connected && this.props.loggedIn;
+
     return (
       <form onSubmit={this._handleSubmit}>
-        <div className="form-group">
-          <label>Handle</label>
-          <input type="text" className="form-control"
-            id="inputHandle" value={this.state.handle}
-            onChange={this._handleHandle} placeholder="Enter handle" />
+        <div className="form-group row">
+          <label className="col-sm-3 col-form-label d-flex justify-content-start align-items-center">handle</label>
+          <div className="col-sm-9">
+            <input type="text" className="form-control"
+              id="inputHandle" value={this.state.handle} disabled={!allow}
+              onChange={this._handleHandle} placeholder="enter handle" />
+          </div>
         </div>
-        <div className="form-group">
-          <label>Email address</label>
-          <input type="email" className="form-control"
-            id="inputEmail" value={this.state.email}
-            onChange={this._handleEmail} placeholder="Enter email" />
+        <div className="form-group row">
+          <label className="col-sm-3 col-form-label d-flex justify-content-start align-items-center">email</label>
+          <div className="col-sm-9">
+            <input type="email" className="form-control d-flex justify-content-start"
+              id="inputEmail" value={this.state.email} disabled={!allow}
+              onChange={this._handleEmail} placeholder="enter email" />
+          </div>
         </div>
-        <input type="submit" className="btn btn-primary" value="Submit" />
+        <div className="form-group row">
+          <div className="col-sm-3"></div>
+          <div className="col-sm-9 d-flex justify-content-start">
+            <input type="submit" disabled={!allow} className="btn btn-primary Login-register-btn" value="Register" />
+          </div>
+        </div>
       </form>
     );
   }
@@ -46,23 +72,53 @@ export default class Login extends Component {
 
     if (connected) {
       if (loggedIn) {
-        info = "Connected to Metamask";
+        info = (
+          <div className="Login-issue-green">
+            <i className="fa fa-check-circle Login-green"></i>
+            <span className="Login-green">
+              {"Connected to Metamask"}
+            {info}
+            </span>
+          </div>
+        );
+
       } else {
-        info = "Please log on to Metamask";
+        info = (
+          <div className="Login-issue">
+            <i className="fa fa-exclamation-circle Login-red"></i>
+            <span className="Login-red">
+              {"Please log on to Metamask"}
+            {info}
+            </span>
+          </div>
+        );
       }
     } else {
-      info = "Metamask not found. Download Metamask extension to get started.";
+      info = (
+          <div className="Login-issue">
+            <i className="fa fa-exclamation-circle Login-red"></i>
+            <span className="Login-red">
+              {"Metamask not found. Download Metamask extension to get started."}
+            {info}
+            </span>
+          </div>
+        );
+
     }
 
     return (
       <div id="gradient" className="Login-background container-fluid">
         <div className="row justify-content-center">
-          <div className="Login-box col-6">
-            <div className="Login-issue">
-              <i className="fa fa-exclamation-circle Login-red"></i>
-              <span className="Login-red">{info}</span>
+          <div className="Login-box col-8">
+            {info}
+            <div className="row">
+              <div className="col">
+                {this._registerInfo()}
+              </div>
+              <div className="col">
+                {this._registerForm()}
+              </div>
             </div>
-            {this._registerForm()}
           </div>
         </div>
       </div>
